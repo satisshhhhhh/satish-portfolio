8e3bb0cb38d01535997cff53c1569caa0a50caa5
satisshhhhhh <satishpanchal651@gmail.com>
2023-09-29 01:17:50 +0530

    Co-authored-by: Harsh Mathur <harshmathurx@users.noreply.github.com> Co-authored-by: Poojan-20 <Poojan-20@users.noreply.github.com>

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Navbar from "@/components/Navbar";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-[#121212] text-white">
+        <Navbar />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,89 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Card } from "@/components/Card";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="px-10 flex flex-col w-full h-full justify-center items-center">
+      <div>
+        <p className="text-md text-center">Hi, My name is</p>
+        <h2 className="text-4xl font-bold">Satish Panchal</h2>
+      </div>
+      <a href={"/satish_panchal_resume.pdf"} target={"_blank"} rel="noreferrer">
+        <button className="bg-AAprimary text-AAsecondary border rounded px-4 sm:px-8 py-3 sm:py-4 border-AAsecondary flex flex-col mt-5">
+          Check Out My Resume !
+        </button>
+      </a>
+
+      <div className="w-full my-5 flex flex-row flex-wrap">
+        <div className="w-full md:w-[50%] p-3">
+          <Card>
+            <div className="flex flex-col p-5">
+              <div className="flex flex-row w-full justify-between items-center">
+                <Link
+                  href="/https://thewalls.vercel.app/"
+                  target="_blank"
+                  className="text-lg font-bold"
+                >
+                  THE WALLS
+                </Link>
+                <p className="text-xs">September 2023</p>
+              </div>
+              <div className="text-zinc-300 mt-1">
+                ● Developed a dynamic web application utilizing Spotify API to
+                curate personalized album walls for users.
+                <br />
+                ● Implemented three distinct categories - Last Month, 6 Months &
+                All Time - to showcase users listening history. <br />● Enabled
+                seamless customization of album wall backgrounds, offering a
+                selection of predefined gradients or the option to upload
+                personal photos for a tailored aesthetic.
+              </div>
+            </div>
+          </Card>
+        </div>
+        <div className="w-full md:w-[50%] p-3">
+          <Card>
+            <div className="flex flex-col p-5">
+              <div className="flex flex-row w-full justify-between items-center">
+                <Link href="https://mithibaitechspark.in/" target="_blank">
+                  <p className="text-lg font-bold">TECHSPARK</p>
+                </Link>
+                <p className="text-xs">February 2023</p>
+              </div>
+              <div className="text-zinc-300 mt-1">
+                ● Implemented an intuitive & user-friendly interface for event
+                registration for the annual technical fest of Mithibai. <br />●
+                Utilized Firebase real-time database for efficient data
+                management and retrieval, ensuring a seamless and smooth event
+                registration process for participants.
+                <br /> ● Contributed to the overall success of the technical
+                festival by providing an efficient registration process for
+                participants.
+              </div>
+            </div>
+          </Card>
+        </div>
+        <div className="w-full md:w-[50%] p-3">
+          <Card>
+            <div className="flex flex-col p-5">
+              <div className="flex flex-row w-full justify-between items-center">
+                <p className="text-lg font-bold">FITNESS FACTORY</p>
+                <p className="text-xs">September 2022</p>
+              </div>
+              <div className="text-zinc-300 mt-1">
+                ● Fitness Application that helps users discover and learn
+                exercises, with a focus on ease of use and accessibility. <br />
+                ● Incorporated YouTube videos that demonstrate exercises and
+                target specific muscle groups, making it easier for users to
+                understand and perform exercises correctly.
+                <br /> ● Delivered a well-rounded solution that combines
+                information, visualization, and user engagement to make fitness
+                accessible to everyone.
+              </div>
+            </div>
+          </Card>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
